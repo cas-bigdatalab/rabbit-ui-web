d54dab7d03aa83ce89df2f83d988b7f9608954dc
lidonggegit <lidongge23@126.com>
2018-11-13 17:06:40 +0800

    Data source page loaded successfully

--- src/service/uitil/util.ts
+++ src/service/uitil/util.ts
@@ -6,14 +6,17 @@ export default class Util extends Vue {
     private POST_HOST = 'http://10.0.88.2:800/api';
 
     post(url: any, data: any, options: any = {}) {
-        let Base64 = require('js-base64').Base64;
-        return this.$http.post(this.POST_HOST + url, data, {
-            headers: {
-                Authorization: 'Basic ' + Base64.encode('admin:admin'),
-                'Access-Control-Allow-Origin': '*'
-            },
-            emulateHTTP: true,
-            emulateJSON: true
+        return axios.post(this.POST_HOST + url, data, {
+            auth: {
+                username: 'admin',
+                password: 'admin'
+            }
+            // headers: {
+            //     Authorization: 'Basic ' + Base64.encode('admin:admin'),
+            //     'Access-Control-Allow-Origin': '*'
+            // },
+            // emulateHTTP: true,
+            // emulateJSON: true
         });
     }
 
@@ -27,24 +30,21 @@ export default class Util extends Vue {
         //     emulateHTTP: true,
         //     emulateJSON: true
         // });
+        return axios.get(this.HOST + url, data ={
+            auth: {
+                username: 'admin',
+                password: 'admin'
+            }
+        })
+    }
+
+    dir_get(url:any){
         return axios.get(this.HOST + url, data={
             auth: {
                 username: 'admin',
                 password: 'admin'
             }
-        });
-    }
-
-    dir_get(url:any){
-        let Base64 = require('js-base64').Base64;
-        return this.$http.get( url, {},{
-            headers: {
-                Authorization: 'Basic ' + Base64.encode('admin:admin'),
-                'Access-Control-Allow-Origin': '*'
-            },
-            emulateHTTP: true,
-            emulateJSON: true
-        });
+        })
     }
 }
 
